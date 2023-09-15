600b29132a17b3bdf5657f8bf96a82983eef3a1d
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-09-15 23:44:36 +0530

    Median-of-two-sorted-arrays

--- Array/Median-of-two-sorted-arrays.java
+++ Array/Median-of-two-sorted-arrays.java
@@ -6,6 +6,7 @@ The overall run time complexity should be O(log (m+n)).
 Example 1:
 Input: nums1 = [1,3], nums2 = [2]
 Output: 2.00000
+
 Explanation: merged array = [1,2,3] and median is 2.
 
 Example 2:
@@ -14,7 +15,6 @@ Output: 2.50000
 
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
  
-
 Constraints:
 
 nums1.length == m
@@ -23,6 +23,7 @@ nums2.length == n
 0 <= n <= 1000
 1 <= m + n <= 2000
 -106 <= nums1[i], nums2[i] <= 106
+
 */
 
 class Solution {
