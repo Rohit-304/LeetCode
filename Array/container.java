64e080f36a6e9b3fd1fc03d757d85cc139767d7d
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-09-16 00:32:42 +0530

    container with most water

--- Array/container.java
+++ Array/container.java
@@ -28,3 +28,30 @@ n == height.length
 0 <= height[i] <= 104
 
 */
+
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right = height.length -1;
+        int max=0;
+        while(left < right){
+            int w = right -left;
+            int h = Math.min(height[left],height[right]);
+            int area = w*h;
+            max = Math.max(max,area);
+            if(height[left]<height[right]){
+                left ++;
+            }
+            else if (height[left]> height[right]){
+                right --;
+            }
+            else{
+                left ++;
+                right --;
+            }
+
+        }
+        return max;
+    }
+}
