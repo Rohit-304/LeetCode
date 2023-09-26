6eb7810d0e499451bf612814de20937e98364cdd
Rohit Sahu <113296241+Rohit-304@users.noreply.github.com>
2023-09-26 23:56:30 +0530

    Update Shuffle the array.java

--- Array/Shuffle the array.java
+++ Array/Shuffle the array.java
@@ -1,4 +1,5 @@
-/*Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+/*
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 Example 1:
 Input: nums = [2,5,1,3,4,7], n = 3
 Output: [2,3,5,4,1,7] 
